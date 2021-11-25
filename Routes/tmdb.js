7a2668f1a281db5df9c9250cc30377fcaba97c25
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-11-25 04:40:27 -0800

    Initial Commit

---
+++ Routes/tmdb.js
@@ -0,0 +1,42 @@
+const cheerio = require("cheerio");
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/tmdb', async (req, res) => {
+    const link = req.query.link;
+    console.log('Link Hit : ', link);
+    console.log(link ? 'Yes' : 'No');
+    if(link){
+        try{
+            const page = await got(link);
+            const $ = cheerio.load(page.body);
+
+            const src = $('#original_header > div.poster_wrapper.false > div > div.image_content.backdrop > img').attr('src');
+            const imgSrc = 'https://themoviedb.org' + (src.replace('_filter(blur)', ''));
+            const title = $('#original_header > div.header_poster_wrapper.false > section > div.title.ott_false > h2 > a');
+            const tagline = $('#original_header > div.header_poster_wrapper.false > section > div.header_info > h3.tagline');
+            const disc = $('#original_header > div.header_poster_wrapper.false > section > div.header_info > div > p');
+            const genres = $('#original_header > div.header_poster_wrapper.false > section > div.title.ott_false > div > span.genres').text().trim();
+            const rating = $('.user_score_chart').first().data('percent');//Get rating of a movie
+
+            const data = {
+                imgsrc : imgSrc,
+                genre : genres,
+                title : title.text(),
+                disc : disc.text(),
+                tagline : tagline.text(),
+                rating : rating,
+            }
+
+            res.status(200).json(data);
+        }catch(err){
+            console.log(err.message);
+            response.status(500).json({ msg : err.message });
+        }
+    }else{
+        res.status(404).json({msg : 'Invalid Link'});
+    }
+})
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const got = require("got");
+var cors = require('cors')
+const app = express();
+
+//Get Route Handlers
+const usersRouter = require('./Routes/users');
+const tmdbRouter = require('./Routes/tmdb');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+//Handle Routes
+app.use('/users', usersRouter);
+app.use('/tmdb', tmdbRouter);
+
+app.get('/', (req, res) => {
+    console.log('Endpoint Hit');
+    res.send("Welcome to Movieto Api");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Started on Port ${PORT}`);
+});
